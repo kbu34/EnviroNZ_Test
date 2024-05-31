ac8e448e69984db6b0d163cb0e5aed379304c2a6
phillip <Ho100600>
2024-05-31 12:57:30 +1200

    first commit

---
+++ aspnet-api/aspnet-api/Location.cs
@@ -0,0 +1,29 @@
+namespace Finder
+{
+    // Implement classes to recieve request and send response
+
+    public class Location
+    {
+        private float longitude { get; set; }
+
+        private float latitude { get; set; }
+
+        private list dataList { set; }
+
+        // static list<Suburb> JsonToList(Json jsonData)
+        // {
+        //      // take the json data and turn it into list
+        //      // return the list
+        // }
+
+        // static Suburb FindClosestSuburb()
+        // {
+        //      Using given coordniates, loop through the list to find the closest
+        //      return the suburb with shortest distance
+        // }
+        }
+
+    }
+
+
+}
--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -1,23 +1,49 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    longitude: "",
+    latitude: ""
+  })
+
+  const handleChange = (event: { target: { name: any; value: any } }) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <div className="m-2">
+          <label>Latitude: </label>
+          <input 
+            className="border-solid border-2"
+            type="text" 
+            name="latitude"
+            value={formData.latitude}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="m-2">
+          <label>Longitude: </label>
+          <input 
+            className="border-solid border-2"
+            type="text" 
+            name="longitude"
+            value={formData.longitude}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button className="rounded-md w-14 mt-2" type="submit">Submit</button>
+      </form>
     </div>
   );
 }
